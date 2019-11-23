97a9efe78de223c5f95f3390490a5c5f0758453a
Anton Wang <antonwang88@gmail.com>
2019-11-22 20:21:26 -0800

    change file type from html to js

---
+++ Basic_JavaScript1.js
@@ -0,0 +1,5 @@
+// Contents are taken from freecodecamp Basic JavaScript tutorials. This is only for my learning purpose.
+
+var myStr; // Change this line 
+myStr="FirstLine\n\t\\SecondLine\nThirdLine"  //FirstLinenewlinetabbackslashSecondLinenewlineThirdLine
+
