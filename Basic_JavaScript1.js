33d62899e80e1d10ea0c81a4b8c4094c6269541a
Anton Wang <antonwang88@gmail.com>
2019-11-24 23:28:59 -0800

    update

--- Basic_JavaScript1.js
+++ Basic_JavaScript1.js
@@ -217,3 +217,229 @@ addSum(3); // sum will be modified but returned value is undefined
 
 //addSum is a function without a return statement. The function 
 //will change the global sum variable but the returned value of the function is undefined.
+
+//Basic JavaScript: Stand in Line
+/*
+In Computer Science a queue is an abstract Data Structure where items are kept in order. 
+New items can be added at the back of the queue and old items are taken off from the front of the queue.
+
+Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+
+Add the number to the end of the array, then remove the first element of the array.
+
+The nextInLine function should then return the element that was removed.
+*/
+function nextInLine(arr, item) {
+  arr.push(item);
+  var remove = arr.shift();
+  return remove; 
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr)); //"Before: [1,2,3,4,5]"
+console.log(nextInLine(testArr, 6)); //1
+console.log("After: " + JSON.stringify(testArr)); //"After: [2,3,4,5,6]"
+
+//Understanding Boolean Values
+/*
+Another data type is the Boolean. 
+Booleans may only be one of two values: true or false. 
+They are basically little on-off switches, where true is "on" and false is "off." 
+These two states are mutually exclusive.
+
+Note: Boolean values are never written with quotes. 
+The strings "true" and "false" are not Boolean and have no special meaning in JavaScript.
+*/
+function welcomeToBooleans() {
+  return true; // return boolean value is true
+}
+
+//Use Conditional Logic with If Statements
+/*
+Pseudocode
+
+if (condition is true) {
+  statement is executed
+}
+ */
+function test (myCondition) {
+  if (myCondition) {
+     return "It was true";
+  }
+  return "It was false";
+}
+test(true);  // returns "It was true"
+test(false); // returns "It was false"
+
+//Comparison with the Equality Operator
+/*
+The most basic operator is the equality operator ==.
+ The equality operator compares two values and 
+ returns true if they're equivalent or false 
+ if they are not. Note that equality is different 
+ from assignment (=), which assigns the value at the
+  right of the operator to a variable in the left.
+ */
+function equalityTest(myVal) {
+  if (myVal == 10) {
+     return "Equal";
+  }
+  return "Not Equal";
+}
+ /* In order for JavaScript to compare two different 
+ data types (for example, numbers and strings), 
+ it must convert one type to another. 
+ This is known as "Type Coercion". Once it does, 
+ however, it can compare terms as follows:*/
+1   ==  1   // true
+1   ==  2   // false
+1   == '1'  // true
+"3" ==  3   // true
+
+//Comparison with the Strict Equality Operator
+/*
+Strict equality (===) is the counterpart to the equality 
+operator (==). However, unlike the equality operator,
+ which attempts to convert both values being compared 
+ to a common type, the strict equality operator does not
+  perform a type conversion.
+
+If the values being compared have different types, 
+they are considered unequal, and the strict equality 
+operator will return false.
+
+Examples */
+3 ===  3   // true
+3 === '3'  // false
+//In the second example, 3 is a Number type and '3' is a String type.
+
+//
+/* 
+If the values being compared are not of the same type, the equality operator will perform a type conversion, and then evaluate the values. However, the strict equality operator will compare both the data type and value as-is, without converting one type to the other.
+
+Examples*/
+3 == '3'  // returns true because JavaScript performs type conversion from string to number
+3 === '3' // returns false because the types are different and type conversion is not performed
+
+//Note: In JavaScript, you can determine the type of a variable or a value with the typeof operator, as follows:
+typeof 3   // returns 'number'
+typeof '3' // returns 'string'
+
+//Comparison with the Inequality Operator
+/*The inequality operator (!=) is the opposite of the 
+equality operator. It means "Not Equal" and returns 
+false where equality would return true and vice versa. 
+Like the equality operator, the inequality operator will
+convert data types of values while comparing. 
+Examples*/
+1 !=  2     // true
+1 != "1"    // false
+1 != '1'    // false
+1 != true   // false
+0 != false  // false
+
+//Comparison with the Strict Inequality Operator
+/*The strict inequality operator (!==) is the logical
+ opposite of the strict equality operator. 
+ It means "Strictly Not Equal" and returns false where
+  strict equality would return true and vice versa. 
+  Strict inequality will not convert data types.
+ Examples */
+3 !==  3   // false
+3 !== '3'  // true
+4 !==  3   // true
+
+//Comparison with the Greater Than Operator
+/*The greater than operator (>) compares the values of
+ two numbers. If the number to the left is greater than
+  the number to the right, it returns true. Otherwise, 
+  it returns false.
+
+Like the equality operator, greater than operator will
+ convert data types of values while comparing.
+
+Examples*/
+5   >  3   // true
+7   > '3'  // true
+2   >  3   // false
+'1' >  9   // false
+
+function testGreaterThan(val) {
+  if (val > 100) {  
+    return "Over 100";
+  }
+
+  if (val > 10) {  
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+testGreaterThan(101);
+
+//Comparison with the Greater Than Or Equal To Operator
+/* 
+Like the equality operator, greater than or equal to 
+operator will convert data types while comparing.
+
+Examples */
+
+6   >=  6   // true
+7   >= '3'  // true
+2   >=  3   // false
+'7' >=  9   // false*/
+
+//Comparison with the Less Than Operator
+/* 
+ Like the equality operator, less than
+ operator converts data types while comparing. */
+2   < 5  // true
+'3' < 7  // true
+5   < 5  // false
+3   < 2  // false
+'8' < 4  // false
+
+function testLessThan(val) {
+  if (val < 25) {
+    return "Under 25";
+  }
+
+  if (val <55) {
+    return "Under 55";
+  }
+  
+  return "55 or Over";
+}
+
+testLessThan(55);
+
+//Comparison with the Less Than Or Equal To Operator
+//Like the equality operator, less than or equal to converts data types.
+//Examples
+4   <= 5  // true
+'7' <= 7  // true
+5   <= 5  // true
+3   <= 2  // false
+'8' <= 4  // false
+
+//Comparisons with the Logical And Operator
+function testLogicalAnd(val) {
+
+  if (val >=25 && val <= 50) {
+    return "Yes";
+  }
+  return "No";
+}
+testLogicalAnd(30); // returns "Yes"
+
+//Comparisons with the Logical Or OperatorPassed
+function testLogicalOr(val) {
+  if (val < 10 || val >20) {
+    return "Outside";
+  }
+  return "Inside";
+}
+testLogicalOr(15); // returns inside
