38e5859120be20e7444e17fda69bb8b296f52a6b
Anton Wang <antonwang88@gmail.com>
2019-11-29 21:56:44 -0800

    adds do...while loops, recursion

--- Basic_JavaScript3.js
+++ Basic_JavaScript3.js
@@ -101,3 +101,213 @@ for (var i = 9; i > 0; i -= 2) {
 }
 
 
+//Iterate Through an Array with a For Loop
+/*A common task in JavaScript is to iterate through the contents of an array. 
+One way to do that is with a for loop. 
+This code will output each element of the array arr to the console: */
+
+var arr = [10,9,8,7,6];
+for (var i = 0; i < arr.length; i++) {
+   console.log(arr[i]);
+}
+/*Remember that arrays have zero-based indexing, which means 
+the last index of the array is length - 1. Our condition for 
+this loop is i < arr.length, which stops the loop when i is equal
+ to length. In this case the last iteration is i === 4 
+ i.e. when i becomes equal to arr.length and outputs 6 to the console. */
+
+// Example
+var myArr = [ 2, 3, 4, 5, 6];
+var total = 0;
+
+for (var i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}   //totals the values inside myArr
+
+//Nesting For Loops
+/*If you have a multi-dimensional array, you can use the same 
+logic as the prior waypoint to loop through both the array and 
+any sub-arrays. Here is an example:  */
+
+var arr = [
+  [1,2], [3,4], [5,6]
+];
+for (var i=0; i < arr.length; i++) {
+  for (var j=0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
+/* This outputs each sub-element in arr one at a time. Note that
+ for the inner loop, we are checking the .length of arr[i], 
+  since arr[i] is itself an array. */
+
+//Modify function multiplyAll so that it multiplies the product 
+//variable by each number in the sub-arrays of arr
+function multiplyAll(arr) {
+  var product = 1;
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];  
+    }
+  }
+   return product;
+}
+multiplyAll([[1,2],[3,4],[5,6,7]]);
+
+//Iterate with JavaScript Do...While Loops
+/*The next type of loop you will learn is called a do...while loop.
+ It is called a do...while loop because it will first do one pass 
+ of the code inside the loop no matter what, and then continue to
+  run the loop while the specified condition evaluates to true.
+*/
+var ourArray = [];
+var i = 0;
+do {
+  ourArray.push(i);
+  i++;
+} while (i < 5);
+/*The example above behaves similar to other types of loops, and the
+ resulting array will look like [0, 1, 2, 3, 4]. However, what makes
+  the do...while different from other loops is how it behaves when 
+  the condition fails on the first check. Let's see this in action: 
+  Here is a regular while loop that will run the code in the loop as
+   long as i < 5: */
+
+var ourArray = []; 
+var i = 5;
+while (i < 5) {
+  ourArray.push(i);
+  i++;
+}
+/* In this example, we initialize the value of ourArray to an empty 
+array and the value of i to 5. When we execute the while loop, the 
+condition evaluates to false because i is not less than 5, so we do 
+not execute the code inside the loop. The result is that ourArray 
+will end up with no values added to it, and it will still look like 
+[] when all of the code in the example above has completed running.
+ Now, take a look at a do...while loop:  */
+
+var ourArray = []; 
+var i = 5;
+do {
+  ourArray.push(i);
+  i++;
+} while (i < 5);
+/* In this case, we initialize the value of i to 5, just like we did
+ with the while loop. When we get to the next line, there is no
+  condition to evaluate, so we go to the code inside the curly braces
+   and execute it. We will add a single element to the array and then
+    increment i before we get to the condition check. When we finally
+   evaluate the condition i < 5 on the last line, we see that i is
+    now 6, which fails the conditional check, so we exit the loop 
+    and are done. At the end of the above example, the value of 
+    ourArray is [5]. Essentially, a do...while loop ensures that the 
+    code inside the loop will run at least once.  */
+
+var myArray = [];
+var i = 10;
+do {   // the loop will push only the number 10 to myArray
+  myArray.push(i);  
+  i++;
+} while (i < 5);
+
+//Replace Loops using Recursion
+/* Recursion is the concept that a function can be expressed in 
+terms of itself. To help understand this, start by thinking about
+ the following task: multiply the elements from 0 to n inclusive in
+  an array to create the product of those elements. Using a for 
+  loop, you could do this:   */
+function multiply(arr, n) {
+  var product = arr[0];
+  for (var i = 1; i <= n; i++) {
+      product *= arr[i];
+  }
+  return product;
+}
+/*However, notice that multiply(arr, n) == multiply(arr, n - 1) * arr[n]. 
+That means you can rewrite multiply in terms of itself and never need
+ to use a loop.  */
+
+function multiply(arr, n) {
+  if (n <= 0) {
+    return arr[0];
+  } else {
+    return multiply(arr, n - 1) * arr[n];
+  }
+}
+/* The recursive version of multiply breaks down like this. 
+In the base case, where n <= 0, it returns the result, arr[0]. 
+For larger values of n, it calls itself, but with n - 1. 
+That function call is evaluated in the same way, calling multiply
+ again until n = 0. At this point, all the functions can return 
+ and the original multiply returns the answer.
+
+Note: Recursive functions must have a base case when they return
+ without calling the function again (in this example, when n <= 0)
+ , otherwise they can never finish executing.  */
+
+//returns the sum of the elements from 0 to n inclusive in an array arr.
+function sum(arr, n) {  
+  if (n <= 0) {
+    return arr[0];
+  } else {
+    return sum(arr, n - 1) + arr[n];
+  }
+}
+
+//Profile Lookup
+/*We have an array of objects representing different people in our contacts lists.
+
+A lookUpProfile function that takes name and a property (prop) 
+as arguments has been pre-written for you.
+
+The function should check if name is an actual contact's firstName
+ and the given property (prop) is a property of that contact.
+
+If both are true, then return the "value" of that property.
+
+If name does not correspond to any contacts then return "No such contact".
+
+If prop does not correspond to any valid properties of a contact
+ found to match name then return "No such property".   */
+
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+function lookUpProfile(name, prop){
+    for (var i = 0; i < contacts.length; i++) {
+        if (name === contacts[i]["firstName"]) {
+            if (contacts[i].hasOwnProperty(prop)) {
+               return contacts[i][prop];   
+            } else return "No such property";
+        } 
+    }
+    return "No such contact";
+}
+lookUpProfile("Akira", "likes");
+
+
