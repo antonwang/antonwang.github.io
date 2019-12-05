77bde99e72505ced128d7038b6a49839c38f787c
Anton Wang <antonwang88@gmail.com>
2019-12-04 22:00:59 -0800

    added a few more tutorials... 2 left

--- Basic_Algorithm_Scripting.js
+++ Basic_Algorithm_Scripting.js
@@ -290,3 +290,90 @@ console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));  // [4,1,2,3,5,6]
 /*Remove all falsy values from an array.
 Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
 Hint: Try converting each value to a Boolean.  */
+
+function bouncer(arr) {
+  let newarr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+        newarr.push(arr[i]);
+     }
+  }
+  return newarr;
+}
+
+console.log(bouncer([7, "ate", "", false, 9])); //[7,"ate",9]
+
+// or using filter()
+function bouncer(arr) {
+  return arr.filter(Boolean);  // filters out falsey of the arr
+}
+
+//Basic Algorithm Scripting: Where do I Belong
+/* Return the lowest index at which a value (second argument) should be inserted 
+into an array (first argument) once it has been sorted. The returned value should be
+ a number.
+
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1
+ (index 0), but less than 2 (index 1).
+
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been 
+sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 
+(index 1).  */
+
+function getIndexToIns(arr, num) {
+  if (arr.length == 0) {
+    return 0;
+    }
+  arr.sort(function(a, b){return a-b});
+  
+  if (num > arr[arr.length - 1]) {
+    return arr.length;
+  }
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (num == arr[i]) {
+      return i;
+      }
+    if (num > arr[i] && num < arr[i+1]) {
+      return i+1;
+      }
+  }
+}
+
+console.log(getIndexToIns([2, 5, 10], 15));
+
+//or a more simplified version that pushes num into arr first before sorting;
+function getIndexToIns(arr, num) {
+  arr.push(num);
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+  return arr.indexOf(num);
+}
+
+//or another simplified version
+function getIndexToIns(arr, num) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  for (var a = 0; a < arr.length; a++) {
+    if (arr[a] >= num) return a;
+  }
+
+  return arr.length;
+}
+
+
+//Basic Algorithm Scripting: Mutations
+/* Return true if the string in the first element of the array contains all of the 
+letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the
+ second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string "hello" does not 
+contain a "y".
+
+Lastly, ["Alien", "line"], should return true because all of the letters in "line" are
+ present in "Alien".   */
