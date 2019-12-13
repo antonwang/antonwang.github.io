4faf539356966a6205e05ca4d2f08c45e353ff0b
Anton Wang <antonwang88@gmail.com>
2019-12-12 16:43:45 -0800

    added more content of theFunctional Programming tutorials

--- Functional_Programming.js
+++ Functional_Programming.js
@@ -507,4 +507,294 @@ var watchList = [
   {"title":"The Dark Knight","rating":"9.0"},{"title":"Batman Begins","rating":"8.3"},
   {"title":"Avatar","rating":"7.9"}]   */
 
+//Functional Programming: Implement map on a Prototype
+/* As you have seen from applying Array.prototype.map(), or simply map() earlier, the map
+method returns an array of the same length as the one it was called on. It also doesn't
+alter the original array, as long as its callback function doesn't.
+
+In other words, map is a pure function, and its output depends solely on its inputs. 
+Plus, it takes another function as its argument.
+
+It would teach us a lot about map to try to implement a version of it that behaves 
+exactly like the Array.prototype.map() with a for loop or Array.prototype.forEach().
+
+Note: A pure function is allowed to alter local variables defined within its scope, 
+although, it's preferable to avoid that as well.
+
+Write your own Array.prototype.myMap(), which should behave exactly like 
+Array.prototype.map(). You may use a for loop or the forEach method.   */
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback){
+  var newArray = [];
+  // Add your code below this line  - done
+  this.forEach(a => newArray.push(callback(a))); //forEach is like a for loop
+  // Add your code above this line
+  return newArray;
+
+};
+
+var new_s = s.myMap(function(item){
+  return item * 2;
+});
+
+//or another solution
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback){
+  var newArray = [];
+  // Add your code below this line - done
+  for (let i = 0; i <this.length; i++) {
+    newArray.push(callback(this[i]));
+}
+  // Add your code above this line
+  return newArray;
+
+};
+
+var new_s = s.myMap(function(item){
+  return item * 2;
+});
+
+
+
+//Functional Programming: Use the filter Method to Extract Data from an Array
+/* Another useful array function is Array.prototype.filter(), or simply filter().
+
+filter calls a function on each element of an array and returns a new array containing
+only the elements for which that function returns true. In other words, it filters 
+the array, based on the function passed to it. Like map, it does this without needing
+to modify the original array.
+
+The callback function accepts three arguments. The first argument is the current 
+element being processed. The second is the index of that element and the third is 
+the array upon which the filter method was called.
+
+See below for an example using the filter method on the users array to return a 
+new array containing only the users under the age of 30. For simplicity, the example
+only uses the first argument of the callback.   */
+
+const users = [
+  { name: 'John', age: 34 },
+  { name: 'Amy', age: 20 },
+  { name: 'camperCat', age: 10 }
+];
+
+const usersUnder30 = users.filter(user => user.age < 30);
+console.log(usersUnder30); // [ { name: 'Amy', age: 20 }, { name: 'camperCat', age: 10 } ]
+
+/* The watchList array holds objects with information on several movies. Use map on
+watchList to assign a new array of objects with only title and rating keys to the 
+ratings variable. The code in the editor currently uses a for loop to do this, so
+you should replace the loop functionality with your map expression.  */
+
+// the global variable
+var watchList = [
+    {
+      "Title": "Inception",
+      "Year": "2010",
+      "Rated": "PG-13",
+      "Released": "16 Jul 2010",
+      "Runtime": "148 min",
+      "Genre": "Action, Adventure, Crime",
+      "Director": "Christopher Nolan",
+      "Writer": "Christopher Nolan",
+      "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+      "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+      "Language": "English, Japanese, French",
+      "Country": "USA, UK",
+      "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+      "Metascore": "74",
+      "imdbRating": "8.8",
+      "imdbVotes": "1,446,708",
+      "imdbID": "tt1375666",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "Interstellar",
+      "Year": "2014",
+      "Rated": "PG-13",
+      "Released": "07 Nov 2014",
+      "Runtime": "169 min",
+      "Genre": "Adventure, Drama, Sci-Fi",
+      "Director": "Christopher Nolan",
+      "Writer": "Jonathan Nolan, Christopher Nolan",
+      "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+      "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+      "Language": "English",
+      "Country": "USA, UK",
+      "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+      "Metascore": "74",
+      "imdbRating": "8.6",
+      "imdbVotes": "910,366",
+      "imdbID": "tt0816692",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "The Dark Knight",
+      "Year": "2008",
+      "Rated": "PG-13",
+      "Released": "18 Jul 2008",
+      "Runtime": "152 min",
+      "Genre": "Action, Adventure, Crime",
+      "Director": "Christopher Nolan",
+      "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+      "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+      "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+      "Language": "English, Mandarin",
+      "Country": "USA, UK",
+      "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+      "Metascore": "82",
+      "imdbRating": "9.0",
+      "imdbVotes": "1,652,832",
+      "imdbID": "tt0468569",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "Batman Begins",
+      "Year": "2005",
+      "Rated": "PG-13",
+      "Released": "15 Jun 2005",
+      "Runtime": "140 min",
+      "Genre": "Action, Adventure",
+      "Director": "Christopher Nolan",
+      "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+      "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+      "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+      "Language": "English, Urdu, Mandarin",
+      "Country": "USA, UK",
+      "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+      "Metascore": "70",
+      "imdbRating": "8.3",
+      "imdbVotes": "972,584",
+      "imdbID": "tt0372784",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "Avatar",
+      "Year": "2009",
+      "Rated": "PG-13",
+      "Released": "18 Dec 2009",
+      "Runtime": "162 min",
+      "Genre": "Action, Adventure, Fantasy",
+      "Director": "James Cameron",
+      "Writer": "James Cameron",
+      "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+      "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+      "Language": "English, Spanish",
+      "Country": "USA, UK",
+      "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+      "Metascore": "83",
+      "imdbRating": "7.9",
+      "imdbVotes": "876,575",
+      "imdbID": "tt0499549",
+      "Type": "movie",
+      "Response": "True"
+    }
+  ];
   
+// Add your code below this line - done
+var list = watchList.filter(item => item["imdbRating"] >= 8.0)
+var filteredList = list.map(item => ({title: item["Title"], rating: item["imdbRating"]}));
+// Add your code above this line
+
+console.log(filteredList);
+/* [ { title: 'Inception', rating: '8.8' },
+{ title: 'Interstellar', rating: '8.6' },
+{ title: 'The Dark Knight', rating: '9.0' },
+{ title: 'Batman Begins', rating: '8.3' } ]  */
+
+
+
+
+// Functional Programming: Implement the filter Method on a Prototype
+/* It would teach us a lot about the filter method if we try to implement a version
+of it that behaves exactly like Array.prototype.filter(). It can use either a for 
+loop or Array.prototype.forEach().
+
+Note: A pure function is allowed to alter local variables defined within its scope, 
+although, it's preferable to avoid that as well.
+
+Write your own Array.prototype.myFilter(), which should behave exactly like 
+Array.prototype.filter(). You may use a for loop or the Array.prototype.forEach() method. */
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback){
+  var newArray = [];
+  // Add your code below this line
+  this.forEach(a => {     //using ES6 syntax  function (a)  is just   a
+    if (callback(a) == true) {
+      newArray.push(a);
+    }
+  });
+  // Add your code above this line
+  return newArray;
+};
+
+var new_s = s.myFilter(function(item){
+  return item % 2 === 1;
+});
+
+//or another solution using for loop
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback){
+  var newArray = [];
+  // Add your code below this line
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i]) === true) {
+      newArray.push(this[i]);
+    }
+  }
+  // Add your code above this line
+  return newArray;
+};
+
+var new_s = s.myFilter(function(item){
+  return item % 2 === 1;
+});
+
+
+
+//Functional Programming: Return Part of an Array Using the slice Method
+/* The slice method returns a copy of certain elements of an array. It can take
+two arguments, the first gives the index of where to begin the slice, the 
+second is the index for where to end the slice (and it's non-inclusive). If the 
+arguments are not provided, the default is to start at the beginning of the array
+through the end, which is an easy way to make a copy of the entire array. The slice
+method does not mutate the original array, but returns a new one.
+
+Here's an example:  */
+
+var arr = ["Cat", "Dog", "Tiger", "Zebra"];
+var newArray = arr.slice(1, 3);
+// Sets newArray to ["Dog", "Tiger"]
+
+/*Use the slice method in the sliceArray function to return part of the anim array
+given the provided beginSlice and endSlice indices. The function should return an array. */
+
+function sliceArray(anim, beginSlice, endSlice) {
+    // Add your code below this line  - done
+    let arr = [...anim];
+    return arr.slice(beginSlice, endSlice);
+    // Add your code above this line
+}
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+console.log(sliceArray(inputAnim, 1, 3)); //prints  [ 'Dog', 'Tiger' ]
