b40298d3ba8c9647edf1d017917fcc0b4bd55ea0
Anton Wang <antonwang88@gmail.com>
2019-12-14 22:04:39 -0800

    Added tutorials on sort method of JavaScript

--- Functional_Programming.js
+++ Functional_Programming.js
@@ -942,3 +942,239 @@ Recall from prior challenges how to filter data and map over it to pull what you
 You may need to create other variables, and return the average rating from getRating
 function. Note that the rating values are saved as strings in the object and need 
 to be converted into numbers before they are used in any mathematical operations. */
+
+// the global variable
+var watchList = [
+  {
+    "Title": "Inception",
+    "Year": "2010",
+    "Rated": "PG-13",
+    "Released": "16 Jul 2010",
+    "Runtime": "148 min",
+    "Genre": "Action, Adventure, Crime",
+    "Director": "Christopher Nolan",
+    "Writer": "Christopher Nolan",
+    "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+    "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+    "Language": "English, Japanese, French",
+    "Country": "USA, UK",
+    "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    "Metascore": "74",
+    "imdbRating": "8.8",
+    "imdbVotes": "1,446,708",
+    "imdbID": "tt1375666",
+    "Type": "movie",
+    "Response": "True"
+  },
+  {
+    "Title": "Interstellar",
+    "Year": "2014",
+    "Rated": "PG-13",
+    "Released": "07 Nov 2014",
+    "Runtime": "169 min",
+    "Genre": "Adventure, Drama, Sci-Fi",
+    "Director": "Christopher Nolan",
+    "Writer": "Jonathan Nolan, Christopher Nolan",
+    "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+    "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    "Language": "English",
+    "Country": "USA, UK",
+    "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+    "Metascore": "74",
+    "imdbRating": "8.6",
+    "imdbVotes": "910,366",
+    "imdbID": "tt0816692",
+    "Type": "movie",
+    "Response": "True"
+  },
+  {
+    "Title": "The Dark Knight",
+    "Year": "2008",
+    "Rated": "PG-13",
+    "Released": "18 Jul 2008",
+    "Runtime": "152 min",
+    "Genre": "Action, Adventure, Crime",
+    "Director": "Christopher Nolan",
+    "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+    "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+    "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+    "Language": "English, Mandarin",
+    "Country": "USA, UK",
+    "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+    "Metascore": "82",
+    "imdbRating": "9.0",
+    "imdbVotes": "1,652,832",
+    "imdbID": "tt0468569",
+    "Type": "movie",
+    "Response": "True"
+  },
+  {
+    "Title": "Batman Begins",
+    "Year": "2005",
+    "Rated": "PG-13",
+    "Released": "15 Jun 2005",
+    "Runtime": "140 min",
+    "Genre": "Action, Adventure",
+    "Director": "Christopher Nolan",
+    "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+    "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+    "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+    "Language": "English, Urdu, Mandarin",
+    "Country": "USA, UK",
+    "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+    "Metascore": "70",
+    "imdbRating": "8.3",
+    "imdbVotes": "972,584",
+    "imdbID": "tt0372784",
+    "Type": "movie",
+    "Response": "True"
+  },
+  {
+    "Title": "Avatar",
+    "Year": "2009",
+    "Rated": "PG-13",
+    "Released": "18 Dec 2009",
+    "Runtime": "162 min",
+    "Genre": "Action, Adventure, Fantasy",
+    "Director": "James Cameron",
+    "Writer": "James Cameron",
+    "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+    "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    "Language": "English, Spanish",
+    "Country": "USA, UK",
+    "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+    "Metascore": "83",
+    "imdbRating": "7.9",
+    "imdbVotes": "876,575",
+    "imdbID": "tt0499549",
+    "Type": "movie",
+    "Response": "True"
+  }
+];
+
+function getRating(watchList){
+  // Add your code below this line
+  var list = watchList.filter(item => item["Director"] === "Christopher Nolan");
+  var length = watchList.filter(item => item["Director"] === "Christopher Nolan").length;
+  var averageRating = list.reduce((sum, movies) => sum + parseFloat(movies["imdbRating"]), 0) / length; 
+
+  // Add your code above this line
+  return averageRating;
+}
+console.log(getRating(watchList));
+
+//Or another solution from freeCodecCamp
+function getRating(watchList){
+  var count = 0;
+  var averageRating = watchList.reduce((sum,movie) =>  {
+    if (movie.Director == "Christopher Nolan") {
+      count+=1;
+      return sum + parseFloat(movie.imdbRating);
+    }
+    return sum;
+  }, 0) / count;
+  return averageRating;
+}
+console.log(getRating(watchList));
+
+//Or another solution from freeCodeCamp
+function getRating(watchList){
+  var averageRating =
+  watchList
+    // Use filter to find films directed by Christopher Nolan
+    .filter(film => film.Director === "Christopher Nolan")
+    // Use map to convert their ratings from strings to numbers
+    .map(film => Number(film.imdbRating))
+    // Use reduce to add together their ratings
+    .reduce((sumOfRatings, rating) => sumOfRatings + rating) /
+  // Divide by the number of Nolan films to get the average rating
+  watchList.filter(film => film.Director === "Christopher Nolan").length;
+  return averageRating;
+}
+console.log(getRating(watchList));
+
+
+
+//Functional Programming: Use Higher-Order Functions map, filter, or reduce 
+//to Solve a Complex Problem
+
+/* Now that you have worked through a few challenges using higher-order functions
+like map(), filter(), and reduce(), you now get to apply them to solve a more 
+complex challenge.
+
+We have defined a function named squareList. You need to complete the code for the
+squareList function using any combination of map(), filter(), and reduce() so that
+it returns a new array containing only the square of only the positive integers 
+(decimal numbers are not integers) when an array of real numbers is passed to it.
+An example of an array containing only real numbers is [-3, 4.8, 5, 3, -3.2].
+
+Note: Your function should not use any kind of for or while loops or the 
+forEach() function. */
+
+const squareList = (arr) => {
+  // only change code below this line
+  return arr = arr.filter(num => num > 0 && num % 1 ==0).map(num => num * num);
+  // only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);  //returns [25, 9]
+
+
+
+
+//Functional Programming: Sort an Array Alphabetically using the sort Method
+//The sort method sorts the elements of an array according to the callback function.
+
+//For example:
+
+function ascendingOrder(arr) {
+  return arr.sort(function(a, b) {
+    return a - b;
+  });
+}
+ascendingOrder([1, 5, 2, 3, 4]);
+// Returns [1, 2, 3, 4, 5]
+
+function reverseAlpha(arr) {
+  return arr.sort(function(a, b) {
+    return a === b ? 0 : a < b ? 1 : -1;
+  });
+}
+reverseAlpha(['l', 'h', 'z', 'b', 's']);
+// Returns ['z', 's', 'l', 'h', 'b']
+/* JavaScript's default sorting method is by string Unicode point value, which may 
+return unexpected results. Therefore, it is encouraged to provide a callback function 
+to specify how to sort the array items. When such a callback function, normally called
+compareFunction, is supplied, the array elements are sorted according to the return
+value of the compareFunction: If compareFunction(a,b) returns a value less than 0 
+for two elements a and b, then a will come before b. If compareFunction(a,b) returns
+a value greater than 0 for two elements a and b, then b will come before a. If 
+compareFunction(a,b) returns a value equal to 0 for two elements a and b, then a 
+and b will remain unchanged.  */
+
+function alphabeticalOrder(arr) {
+  // Add your code below this line
+  return arr.sort(function(a, b) {
+    return a === b ? 0: a < b ? -1: 1;
+  });
+  // Add your code above this line
+}
+console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"])); 
+//returns [ 'a', 'a', 'c', 'd', 'g', 'z' ]
+
+
+//Functional Programming: Return a Sorted Array Without Changing the Original Array
+/* A side effect of the sort method is that it changes the order of the elements in 
+the original array. In other words, it mutates the array in place. One way to avoid
+this is to first concatenate an empty array to the one being sorted (remember that 
+slice and concat return a new array), then run the sort method.
+
+Use the sort method in the nonMutatingSort function to sort the elements of an array 
+in ascending order. The function should return a new array, and not mutate the 
+globalArray variable.  */
