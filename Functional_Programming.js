a779285169e322318e5c0a73ac7d978fa6bc4b4d
Anton Wang <antonwang88@gmail.com>
2019-12-15 23:59:23 -0800

    added split method

--- Functional_Programming.js
+++ Functional_Programming.js
@@ -1178,3 +1178,39 @@ slice and concat return a new array), then run the sort method.
 Use the sort method in the nonMutatingSort function to sort the elements of an array 
 in ascending order. The function should return a new array, and not mutate the 
 globalArray variable.  */
+
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Add your code below this line
+  let newarr = [...arr];  //this also works  let newarr = arr.slice(0);
+  return newarr.sort(function(a,b) {
+    return a - b;
+  });
+  // Add your code above this line
+}
+console.log(nonMutatingSort(globalArray));  // returns [ 2, 3, 5, 6, 9 ]
+
+
+//Functional Programming: Split a String into an Array Using the split Method
+/* The split method splits a string into an array of strings. It takes an argument 
+for the delimiter, which can be a character to use to break up the string or a regular
+expression. For example, if the delimiter is a space, you get an array of words, and
+if the delimiter is an empty string, you get an array of each character in the string.
+
+Here are two examples that split one string by spaces, then another by digits using a
+regular expression:  */
+
+var str = "Hello World";
+var bySpace = str.split(" ");
+// Sets bySpace to ["Hello", "World"]
+
+var otherString = "How9are7you2today";
+var byDigits = otherString.split(/\d/);
+// Sets byDigits to ["How", "are", "you", "today"]
+
+/* Since strings are immutable, the split method makes it easier to work with them.
+
+Use the split method inside the splitify function to split str into an array of words.
+The function should return the array. Note that the words are not always separated by
+spaces, and the array should not contain punctuation.  */
+
