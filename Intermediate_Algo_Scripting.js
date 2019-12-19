e78a79136c3883efcb39c39578e399ab685a35f9
Anton Wang <antonwang88@gmail.com>
2019-12-18 21:19:58 -0800

    corrected the solution for first challenge

--- Intermediate_Algo_Scripting.js
+++ Intermediate_Algo_Scripting.js
@@ -10,18 +10,34 @@ For example, sumAll([4,1]) should return 10 because sum of all the numbers betwe
 and 4 (both inclusive) is 10. */
 
 function sumAll(arr) {
-    let sum = 0;
-    if (arr[0] < arr[1]) {
-      for (let i=arr[0]; i<=arr[1]; i++) {
-        sum += i;
-      }
+  let sum = 0;
+  if (arr[0] < arr[1]) {
+    for (let i=arr[0]; i<=arr[1]; i++) {
+      sum += i;
     }
-    else
-      for (let i=arr[1]; i>=arr[0]; i--) {
-        sum += i;
-      }
-    return sum;
   }
-  
-  console.log(sumAll([1, 4]));
-  
+  if (arr[0] > arr[1]) {
+    for (let i=arr[1]; i<=arr[0]; i++) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+console.log(sumAll([1, 4]));
+
+//Another solution
+function sumAll(arr) {
+  var sum = 0;
+  for (var i = Math.min(...arr); i <= Math.max(...arr); i++) {
+    sum += i;
+  } //Math.max() finds the maximum value of two numbers, and Math.min finds the min value
+  return sum;
+}
+sumAll([1, 4]);
+
+
+
+//Intermediate Algorithm Scripting: Diff Two Arrays
+/* Compare two arrays and return a new array with any items only found in one of 
+the two given arrays, but not both. In other words, return the symmetric difference 
+of the two arrays.  */
