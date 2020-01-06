05de50f0b13c74dbce025a09bf2d1029c50ae5cd
Anton Wang <antonwang88@gmail.com>
2020-01-06 10:40:07 -0800

    added tutorials: optimize re-renders with shouldComponentUpdate, introducing inline styles, add inline styles in React, use advanced JavaScript in React Render Method, render with an if/else condition

--- React.jsx
+++ React.jsx
@@ -1587,3 +1587,228 @@ class MyComponent extends React.Component {
     }
 };
 
+
+/* React: Optimize Re-Renders with shouldComponentUpdate
+So far, if any component receives new state or new props, it re-renders itself and all its children. This is usually okay. But React provides a lifecycle method you can call when child components receive new state or props, and declare specifically if the components should update or not. The method is shouldComponentUpdate(), and it takes nextProps and nextState as parameters.
+
+This method is a useful way to optimize performance. For example, the default behavior is that your component re-renders when it receives new props, even if the props haven't changed. You can use shouldComponentUpdate() to prevent this by comparing the props. The method must return a boolean value that tells React whether or not to update the component. You can compare the current props (this.props) to the next props (nextProps) to determine if you need to update or not, and return true or false accordingly.
+
+The shouldComponentUpdate() method is added in a component called OnlyEvens. Currently, this method returns true so OnlyEvens re-renders every time it receives new props. Modify the method so OnlyEvens updates only if the value of its new props is even. Click the Add button and watch the order of events in your browser's console as the lifecycle hooks are triggered. */
+
+class OnlyEvens extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+      console.log('Should I update?');
+       // change code below this line
+      if (nextProps.value % 2 == 0) { //updates only if value of nextProp is even
+      return true;
+      }
+      else return false;
+       // change code above this line
+    }
+    componentDidUpdate() {
+      console.log('Component re-rendered.');
+    }
+    render() {
+      return <h1>{this.props.value}</h1>
+    }
+};
+  
+  class Controller extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: 0
+      };
+      this.addValue = this.addValue.bind(this);
+    }
+    addValue() { //simply increment the value of this.state by 1 when called upon
+      this.setState({
+        value: this.state.value + 1
+      });
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.addValue}>Add</button>{/*calls addValue upon button click*/}
+          <OnlyEvens value={this.state.value}/>
+        </div>
+      );
+    }
+};
+  
+
+
+/* React: Introducing Inline Styles
+There are other complex concepts that add powerful capabilities to your React code. But you may be wondering about the more simple problem of how to style those JSX elements you create in React. You likely know that it won't be exactly the same as working with HTML because of the way you apply classes to JSX elements.
+
+If you import styles from a stylesheet, it isn't much different at all. You apply a class to your JSX element using the className attribute, and apply styles to the class in your stylesheet. Another option is to apply inline styles, which are very common in ReactJS development.
+
+You apply inline styles to JSX elements similar to how you do it in HTML, but with a few JSX differences. Here's an example of an inline style in HTML: */
+
+<div style="color: yellow; font-size: 16px">Mellow Yellow</div>
+
+/* JSX elements use the style attribute, but because of the way JSX is transpiled, you can't set the value to a string. Instead, you set it equal to a JavaScript object. Here's an example: */
+
+<div style={{color: "yellow", fontSize: 16}}>Mellow Yellow</div>
+
+/* Notice how we camelCase the "fontSize" property? This is because React will not accept kebab-case keys (a naming style with all lowercase with dashes separating words i.e. font-size) in the style object. React will apply the correct property name for us in the HTML.
+
+Add a style attribute to the div in the code editor to give the text a color of red and font size of 72px.
+
+Note that you can optionally set the font size to be a number, omitting the units "px", or write it as "72px". */
+
+class Colorful extends React.Component {
+    render() {
+      return (
+        <div style={{color: "red", fontSize: 72}}>Big Red</div>
+      );
+    }
+};
+
+
+
+/* React: Add Inline Styles in React
+You may have noticed in the last challenge that there were several other syntax differences from HTML inline styles in addition to the style attribute set to a JavaScript object. First, the names of certain CSS style properties use camel case. For example, the last challenge set the size of the font with fontSize instead of font-size. Hyphenated words like font-size are invalid syntax for JavaScript object properties, so React uses camel case. ***As a rule, any hyphenated style properties are written using camel case in JSX.***
+
+All property value length units (like height, width, and fontSize) are assumed to be in px unless otherwise specified. If you want to use em, for example, you wrap the value and the units in quotes, like {fontSize: "4em"}. Other than the length values that default to px, all other property values should be wrapped in quotes.
+
+If you have a large set of styles, you can assign a style object to a constant to keep your code organized. Uncomment the styles constant and declare an object with three style properties and their values. Give the div a color of "purple", a font-size of 40, and a border of "2px solid purple". Then set the style attribute equal to the styles constant. */
+
+const styles =  {color: "purple", fontSize: 40, border: "2px solid purple"};
+// change code above this line
+class Colorful extends React.Component {
+  render() {
+    // change code below this line
+    return (
+      <div style={styles}>Style Me!</div> //keeping code organize by referencing to styles
+    );
+    // change code above this line
+  }
+};
+
+
+
+/* React: Use Advanced JavaScript in React Render Method
+In previous challenges, you learned how to inject JavaScript code into JSX code using curly braces, { }, for tasks like accessing props, passing props, accessing state, inserting comments into your code, and most recently, styling your components. These are all common use cases to put JavaScript in JSX, but they aren't the only way that you can utilize JavaScript code in your React components.
+
+You can also write JavaScript directly in your render methods, before the return statement, without inserting it inside of curly braces. This is because it is not yet within the JSX code. When you want to use a variable later in the JSX code inside the return statement, you place the variable name inside curly braces.
+
+In the code provided, the render method has an array that contains 20 phrases to represent the answers found in the classic 1980's Magic Eight Ball toy. The button click event is bound to the ask method, so each time the button is clicked a random number will be generated and stored as the randomIndex in state. On line 52, delete the string "change me!" and reassign the answer const so your code randomly accesses a different index of the possibleAnswers array each time the component updates. Finally, insert the answer const inside the p tags. */
+
+const inputStyle = {
+    width: 235,
+    margin: 5
+  }
+  
+  class MagicEightBall extends React.Component { //classic 1980's Magic Eight Ball toy
+    constructor(props) {
+      super(props);
+      this.state = {
+        userInput: '',
+        randomIndex: ''
+      }
+      this.ask = this.ask.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+    }
+    ask() {
+      if (this.state.userInput) { 
+        //if state.userInput has a string (bool=true), generate number and reset user input
+        this.setState({
+          randomIndex: Math.floor(Math.random() * 20),
+          userInput: ''
+        });
+      }
+    }
+    handleChange(event) { //updates user input in state.userInput 
+      this.setState({
+        userInput: event.target.value  
+      });
+    }
+    render() {
+      const possibleAnswers = [
+        'It is certain',
+        'It is decidedly so',
+        'Without a doubt',
+        'Yes, definitely',
+        'You may rely on it',
+        'As I see it, yes',
+        'Outlook good',
+        'Yes',
+        'Signs point to yes',
+        'Reply hazy try again',
+        'Ask again later',
+        'Better not tell you now',
+        'Cannot predict now',
+        'Concentrate and ask again',
+        'Don\'t count on it',
+        'My reply is no',
+        'My sources say no',
+        'Most likely',
+        'Outlook not so good',
+        'Very doubtful'
+      ];
+      const answer = possibleAnswers[this.state.randomIndex]; // outputs value of index
+      return (
+        <div>
+          <input
+            type="text"
+            value={this.state.userInput} 
+            onChange={this.handleChange} //updates userInput when user inputs value
+            style={inputStyle} /><br />
+          {/* reset userInput value and generate randomIndex value */} 
+          <button onClick={this.ask}> 
+            Ask the Magic Eight Ball!
+          </button><br />
+          <h3>Answer:</h3>
+          <p>
+            { /* change code below this line */ }
+          {answer}
+            { /* change code above this line */ }
+          </p>
+        </div>
+      );
+    }
+};
+
+
+/* React: Render with an If-Else Condition
+Another application of using JavaScript to control your rendered view is to tie the elements that are rendered to a condition. When the condition is true, one view renders. When it's false, it's a different view. You can do this with a standard if/else statement in the render() method of a React component.
+
+MyComponent contains a boolean in its state which tracks whether you want to display some element in the UI or not. The button toggles the state of this value. Currently, it renders the same UI every time. Rewrite the render() method with an if/else statement so that if display is true, you return the current markup. Otherwise, return the markup without the h1 element.
+
+Note: You must write an if/else to pass the tests. Use of the ternary operator will not pass here. */
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        display: true
+      }
+      this.toggleDisplay = this.toggleDisplay.bind(this);
+    }
+    toggleDisplay() {
+      this.setState({
+        display: !this.state.display
+      });
+    }
+    render() {
+      // change code below this line
+      if (this.state.display) {
+        return (
+          <div>
+            <button onClick={this.toggleDisplay}>Toggle Display</button>
+            <h1>Displayed!</h1>
+          </div>
+        );
+      }
+      else return (
+         <div>
+            <button onClick={this.toggleDisplay}>Toggle Display</button>
+        </div>
+      );
+    }
+};
+  
