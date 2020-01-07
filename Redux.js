95cda50166dbdeacae7bb34555e0ccd460e5387f
Anton Wang <antonwang88@gmail.com>
2020-01-06 18:37:35 -0800

    added overview of Redux tutorials

---
+++ Redux.js
@@ -0,0 +1,26 @@
+// Contents are taken from freecodecamp's Redux tutorials. 
+//This is only for my learning purpose.
+
+/* Tutorials
+
+Introduction to the Redux Challenges
+Create a Redux Store
+Get State from the Redux Store
+Define a Redux Action
+Define an Action Creator
+Dispatch an Action Event
+Handle an Action in the Store
+Use a Switch Statement to Handle Multiple Actions
+Use const for Action Types
+Register a Store Listener
+Combine Multiple Reducers
+Send Action Data to the Store
+Use Middleware to Handle Asynchronous Actions
+Write a Counter with Redux
+Never Mutate State
+Use the Spread Operator on Arrays
+Remove an Item from an Array
+Copy an Object with Object.assign
+*/
+
+
