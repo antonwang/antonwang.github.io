16c2d8ee25b11e78a3779725c52993ac4f19c561
Anton Wang <antonwang88@gmail.com>
2020-03-01 11:43:40 -0800

    learned about package.json, author, description, keywords, license, dependencies

---  ManagingPackagesNpm.js
+++  ManagingPackagesNpm.js
@@ -51,3 +51,33 @@ One of the most common pieces of information in this file is the author field. I
 Add your name as the author of the project in the package.json file.
 
 Note: Remember that you’re writing JSON, so all field names must use double-quotes (") and be separated with a comma (,). */
+
+/* Managing Packages with Npm - Add a Description to Your package.json
+The next part of a good package.json file is the description field; where a short, but informative description about your project belongs.
+
+If you some day plan to publish a package to npm, this is the string that should sell your idea to the user when they decide whether to install your package or not. However, that’s not the only use case for the description, it’s a great way to summarize what a project does. It’s just as important in any Node.js project to help other developers, future maintainers or even your future self understand the project quickly.
+
+Regardless of what you plan for your project, a description is definitely recommended. Here's an example:
+
+"description": "A project that does something awesome",
+Add a description to the package.json file of your project.
+
+Note: Remember to use double-quotes for field-names (") and commas (,) to separate fields. */
+
+/* Managing Packages with Npm - Add Keywords to Your package.json
+The keywords field is where you can describe your project using related keywords. Here's an example:
+
+"keywords": [ "descriptive", "related", "words" ],
+As you can see, this field is structured as an array of double-quoted strings.
+
+Add an array of suitable strings to the keywords field in the package.json file of your project.
+
+One of the keywords should be "freecodecamp". */
+
+/* Managing Packages with Npm - Add a License to Your package.json
+The license field is where you inform users of what they are allowed to do with your project.
+
+Some common licenses for open source projects include MIT and BSD. License information is not required, and copyright laws in most countries will give you ownership of what you create by default. However, it’s always a good practice to explicitly state what users can and can’t do. Here's an example of the license field:
+
+"license": "MIT",
+Fill the license field in the package.json file of your project as you find suitable. */
