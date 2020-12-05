b7d97b001b7204989cc27c0d84aa21703f353640
Anton Wang <antonwang88@gmail.com>
2020-12-05 10:54:02 -0800

    update notes

--- Intro - Basic Node and Express.js
+++ Intro - Basic Node and Express.js
@@ -55,5 +55,39 @@ Note: All the code for these lessons should be added in between the few lines of
 solution: app.get("/", (req, res) => res.send("Hello Express"));
 */
 
+/* Basic Node and Express - Serve an HTML File
+You can respond to requests with a file using the res.sendFile(path) method. You can put it inside the app.get('/', ...) route handler. Behind the scenes, this method will set the appropriate headers to instruct your browser on how to handle the file you want to send, according to its type. Then it will read and send the file. This method needs an absolute file path. We recommend you to use the Node global variable __dirname to calculate the path like this:
 
+absolutePath = __dirname + relativePath/file.ext
+Send the /views/index.html file as a response to GET requests to the / path. If you view your live app, you should see a big HTML heading (and a form that we will use later…), with no style applied.
+
+Note: You can edit the solution of the previous challenge or create a new one. If you create a new solution, keep in mind that Express evaluates routes from top to bottom, and executes the handler for the first match. You have to comment out the preceding solution, or the server will keep responding with a string. 
+
+solution: 
+var express = require("express");
+var app = express();
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
+module.exports = app;
+*/
+/* 
+Basic Node and Express - Serve Static Assets
+An HTML server usually has one or more directories that are accessible by the user. You can place there the static assets needed by your application (stylesheets, scripts, images). In Express, you can put in place this functionality using the middleware express.static(path), where the path parameter is the absolute path of the folder containing the assets. If you don’t know what middleware is... don’t worry, we will discuss in detail later. Basically, middleware are functions that intercept route handlers, adding some kind of information. A middleware needs to be mounted using the method app.use(path, middlewareFunction). The first path argument is optional. If you don’t pass it, the middleware will be executed for all requests.
+
+Mount the express.static() middleware for all requests with app.use(). The absolute path to the assets folder is __dirname + /public.
+
+Now your app should be able to serve a CSS stylesheet. From outside, the public folder will appear mounted to the root directory. Your front-page should look a little better now!
+
+solution:
+
+var express = require("express");
+var app = express();
+
+console.log("Hello World");
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
+
+app.use(express.static(__dirname + "/public"));
+module.exports = app;
+ */
 
