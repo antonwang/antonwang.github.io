d3e2c1be81276a29e491bb14d74c6476cdcbb615
Anton Wang <antonwang88@gmail.com>
2020-12-08 21:42:30 -0800

    organizing files - create new file

---
+++ MongoDB and Mongoose.js
@@ -0,0 +1,26 @@
+/* Introduction to the MongoDB and Mongoose Challenges
+
+Install and Set Up Mongoose
+
+Create a Model
+
+Create and Save a Record of a Model
+
+Create Many Records with model.create()
+
+Use model.find() to Search Your Database
+
+Use model.findOne() to Return a Single Matching Document from Your Database
+
+Use model.findById() to Search Your Database By _id
+
+Perform Classic Updates by Running Find, Edit, then Save
+
+Perform New Updates on a Document Using model.findOneAndUpdate()
+
+Delete One Document Using model.findByIdAndRemove
+
+Delete Many Documents with model.remove()
+
+Chain Search Query Helpers to Narrow Search Results */
+
