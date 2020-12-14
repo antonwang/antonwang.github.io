34774c4a2de0a520b3d93697676efb9a0c493167
Anton Wang <antonwang88@gmail.com>
2020-12-13 22:05:00 -0800

    update

--- MongoDB and Mongoose.js
+++ MongoDB and Mongoose.js
@@ -137,8 +137,8 @@ const findPeopleByName = (personName, done) => {
   };  //findPeopleByName("Anne", function() {});
 
 /* 
-  MongoDB and Mongoose - Use model.findOne() to Return a Single Matching Document from Your Database
+MongoDB and Mongoose - Use model.findOne() to Return a Single Matching Document from Your Database
 
-  Model.findOne() behaves like .find(), but it returns only one document (not an array), even if there are multiple items. It is especially useful when searching by properties that you have declared as unique.
-  
-  Modify the findOneByFood function to find just one person which has a certain food in the person's favorites, using Model.findOne() -> Person. Use the function argument food as search key. */
+Model.findOne() behaves like .find(), but it returns only one document (not an array), even if there are multiple items. It is especially useful when searching by properties that you have declared as unique.
+
+Modify the findOneByFood function to find just one person which has a certain food in the person's favorites, using Model.findOne() -> Person. Use the function argument food as search key. */
