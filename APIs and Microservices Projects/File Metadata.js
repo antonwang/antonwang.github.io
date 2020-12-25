667a1377ddb557c2e6183950a2bbc06597bb6b03
Anton Wang <antonwang88@gmail.com>
2020-12-24 23:04:01 -0800

    included multer and multer.single(fieldname)

--- APIs and Microservices Projects/File Metadata.js
+++ APIs and Microservices Projects/File Metadata.js
@@ -11,7 +11,6 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
@@ -21,12 +20,16 @@ app.listen(port, function () {
 let multer = require('multer');
 
 //user story #1: You can submit a form that includes a file upload.
-//https://www.npmjs.com/package/multer
-app.post('/api/fileanalyse',multer().single('upfile'), (request, response) => {
-  console.log(request.file)
-  response.json({})
-})
-
 //user story #2: The form file input field has the name attribute set to upfile.
-
 //user story #3: When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
+
+//https://www.npmjs.com/package/multer
+//multer().single(fieldname) - accepts a single file with the name fieldname. The single file will be stored in req.file.
+app.post('/api/fileanalyse', multer().single('upfile'), (request, response) => {
+  //console.log(request.file)  - to view the object in request.file
+  response.json({
+    name: request.file.originalname,
+    type: request.file.mimetype,
+    size: request.file.size
+  })
+})
