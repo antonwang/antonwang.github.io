425dc2bee0ff8ceaf2e772703adb43c00961824b
Anton Wang <antonwang88@gmail.com>
2020-12-26 10:42:56 -0800

    documenting fcc QA module

---
+++ Quality Assurance/QA and testing with Chai.js
@@ -0,0 +1,53 @@
+/* Module overview
+
+Introduction to the Quality Assurance with Chai Challenges
+
+Learn How JavaScript Assertions Work
+
+Test if a Variable or Function is Defined
+
+Use Assert.isOK and Assert.isNotOK
+
+Test for Truthiness
+
+Use the Double Equals to Assert Equality
+
+Use the Triple Equals to Assert Strict Equality
+
+Assert Deep Equality with .deepEqual and .notDeepEqual
+
+Compare the Properties of Two Elements
+
+Test if One Value is Below or At Least as Large as Another
+
+Test if a Value Falls within a Specific Range
+
+Test if a Value is an Array
+
+Test if an Array Contains an Item
+
+Test if a Value is a String
+
+Test if a String Contains a Substring
+
+Use Regular Expressions to Test a String
+
+Test if an Object has a Property
+
+Test if a Value is of a Specific Data Structure Type
+
+Test if an Object is an Instance of a Constructor
+
+Run Functional Tests on API Endpoints using Chai-HTTP
+
+Run Functional Tests on API Endpoints using Chai-HTTP II
+
+Run Functional Tests on an API Response using Chai-HTTP III - PUT method
+
+Run Functional Tests on an API Response using Chai-HTTP IV - PUT method
+
+Run Functional Tests using a Headless Browser
+
+Run Functional Tests using a Headless Browser II
+
+Simulate Actions Using a Headless Browser */
